987fec372207e169c4bb2af20896dc8b72bd4fab
dinesh <dineshk281800@gmail.com>
2023-08-20 20:00:06 +0530

    updated

---
+++ js/form.js
@@ -0,0 +1,10 @@
+// select all children of form element
+
+const form=[...document.querySelector('.form').children];
+
+form.forEach((item,i)=>{
+  // set item's opacity to 1 after some interval
+  setTimeout(()=>{
+    item.style.opacity=1;
+  },i*100);
+})
